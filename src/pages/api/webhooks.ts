00c1ff17c19a36f5812f03de6683c0295907fc89
Bruno Silva <brs.rsilva@gmail.com>
2021-06-21 16:59:10 -0300

    gitignore

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,9 @@
+import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable){
     const chunks = [];
@@ -20,6 +22,11 @@ export const config = {
         bodyParser: false,
     }
 }
+
+const relevantEvents =new Set([
+    'checkout.session.completed'
+])
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method === 'POST'){
         const buf = await buffer(req)
@@ -33,9 +40,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             return res.status(400).send(`Webhook error: ${err.message}`)
         }
 
-        
+        const { type } = event;
 
-        res.status(200).json({ok: true});
+        if(relevantEvents.has(type)){
+            try {
+                switch(type){
+                    case 'checkout.session.completed':
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                        await saveSubscription(
+                            checkoutSession.subscription?.toString(),
+                            checkoutSession.customer?.toString()
+                        )
+
+                        break;
+                    default:
+                        throw new Error('Unhandled event.')
+                }
+            } catch (err) {
+                return res.json({ error: 'Webhook handle failed.'})
+            }
+        }
+
+        res.json({received: true});
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
